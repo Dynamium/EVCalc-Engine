e0a2922b8727412a8e8d69de003049ffa6fd8f50
Егор Яковенко <ggoraa1029@gmail.com>
2020-10-27 16:11:25 +0200

    upd automation

--- .space.kts
+++ .space.kts
@@ -8,6 +8,9 @@ job("Build Engine") {
 
 job("Publish artifact") {
     container("openjdk:14") {
+        env["jb_packages_username"] = Secrets("jb_packages_username")
+        env["jb_packages_password"] = Secrets("jb_packages_password")
+
         shellScript {
             content = "./gradlew publish"
         }
