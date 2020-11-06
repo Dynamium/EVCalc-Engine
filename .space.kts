94f8c090f14e17b63160482f45e886fedbf804b3
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-06 20:59:46 +0200

    Update space automation

--- .space.kts
+++ .space.kts
@@ -1,18 +1,26 @@
-job("Build Engine") {
+job("[Engine] Build") {
     container("openjdk:14") {
         shellScript {
-            content = "./gradlew clean build --info "
+            content = "./gradlew clean build --info"
         }
     }
 }
 
-job("Publish artifact") {
+job("[Engine] Publish artifact") {
     container("openjdk:14") {
         env["jb_packages_username"] = Secrets("jb_packages_username")
         env["jb_packages_password"] = Secrets("jb_packages_password")
 
         shellScript {
             content = "./gradlew publish"
+        }
+    }
+}
+
+job("[Engine] Run tests") {
+    container("openjdk:14") {
+        shellScript {
+            content = "./gradlew test --info"
         }
     }
 }
