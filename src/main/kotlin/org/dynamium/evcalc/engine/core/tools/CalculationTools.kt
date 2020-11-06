61b756174401ded665854308cb3bb8911a5f739a
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-06 20:48:38 +0200

    updated CalculationTools.kt

--- src/main/kotlin/org/dynamium/evcalc/engine/core/tools/CalculationTools.kt
+++ src/main/kotlin/org/dynamium/evcalc/engine/core/tools/CalculationTools.kt
@@ -12,7 +12,17 @@ internal object CalculationTools {
         100 - val1
     }
 
-    fun calculatePercentageFromOneToAnother() {
+    /*
+     * Get offset of one value from another.
+     *
+     * @property num1 First value, needs to be bigger so offset wont be negative
+     * @property num2 Second value, needs to be less than first argument.
+     */
+    fun getOffsetOfValues(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+
+    fun getPercentageOfOneValueFromAnother() {
 
     }
 }
