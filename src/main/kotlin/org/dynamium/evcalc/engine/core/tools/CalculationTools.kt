63a1041a9529f86bc0e35053d3a3f6167afade7c
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-06 18:37:57 +0200

    start work on calculation tools

---
+++ src/main/kotlin/org/dynamium/evcalc/engine/core/tools/CalculationTools.kt
@@ -0,0 +1,16 @@
+package org.dynamium.evcalc.engine.core.tools
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+
+internal object CalculationTools {
+    fun Int.invertPercents() {
+
+    }
+
+    fun calculatePercentageFromOneToAnother() {
+
+    }
+}
