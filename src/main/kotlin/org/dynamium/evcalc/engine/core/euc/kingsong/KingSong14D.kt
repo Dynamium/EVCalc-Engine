eb787d32d5a715679b3bc3cfbdb03038eaac4f8c
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-07 12:44:53 +0200

    Add TODO()s to KingSong14D.kt

--- src/main/kotlin/org/dynamium/evcalc/engine/core/euc/kingsong/KingSong14D.kt
+++ src/main/kotlin/org/dynamium/evcalc/engine/core/euc/kingsong/KingSong14D.kt
@@ -28,11 +28,12 @@ private const val speedOffset = 1
 
 private val logger = KotlinLogging.logger {}
 
-internal object KS14D {
+internal object KingSong14DCalculation {
 
     fun calculateMileage(riderWeight: Int, batteryCapacity: Int, airTemp: Int, batteryCycles: Int, speed: Int): Int {
         var calculatedValue = startMileage
 
+        TODO()
 
         return calculatedValue
     }
@@ -41,19 +42,19 @@ internal object KS14D {
         var calculatedValue = 0
         when (attribute) {
             Attribute.RIDER_WEIGHT -> {
-
+                TODO()
             }
             Attribute.BATTERY_CAPACITY -> {
-
+                TODO()
             }
             Attribute.AIR_TEMP -> {
-
+                TODO()
             }
             Attribute.BATTERY_CYCLES -> {
-
+                TODO()
             }
             Attribute.SPEED -> {
-
+                TODO()
             }
         }
         return calculatedValue
