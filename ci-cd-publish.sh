6487c414539f7f5d2df64f0c440893e67759ec21
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-08 16:44:56 +0200

    fix the bash script

--- ci-cd-publish.sh
+++ ci-cd-publish.sh
@@ -1,6 +1,6 @@
 # This script publishes the artifacts in needed repositories.
 
-PROJECT_VERSION=./gradlew properties -q | grep "version:" | awk "{print $2}"
+PROJECT_VERSION=$(./gradlew properties -q | grep "version:" | awk "{print $2}")
 
 if grep -q "SNAPSHOT" <<< "$PROJECT_VERSION"; then # If project version contains SNAPSHOT
     ./gradlew publishEvcalcPublicationToMaven2Repository # Publish to a snapshot repository
