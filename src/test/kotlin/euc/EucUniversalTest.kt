ef53da4c48ae3f537d20edfd6751b2e64ec325c8
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-26 12:32:00 +0200

    test refactor, added calculation speed test

--- src/test/kotlin/euc/EucUniversalTest.kt
+++ src/test/kotlin/euc/EucUniversalTest.kt
@@ -1,44 +1,52 @@
 package euc
 
+import executeAndMeasureTimeInMillis
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.ints.shouldBeLessThan
-import io.kotest.matchers.ints.shouldNotBeLessThan
 import io.kotest.matchers.ints.shouldNotBeLessThanOrEqual
+import io.kotest.matchers.longs.shouldNotBeGreaterThan
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
 import org.dynamium.evcalc.engine.api.DeviceModel
 import org.dynamium.evcalc.engine.api.EVCalc
-                                    
+
 class EucUniversalTest : StringSpec({
     val device = DeviceModel.EUC_UNIVERSAL
     "Returned value needs to pe positive" {
         val calculatedValue = EVCalc.calculateMileage(76, 1554, 31, 100, 36, device)
-        calculatedValue.shouldBeGreaterThan(0)
+        calculatedValue shouldBeGreaterThan 0
     }
 
     "When battery capacity is 0, the result needs to be 0" {
         val calculatedValue = EVCalc.calculateMileage(75, 0, 31, 100, 36, device)
-        calculatedValue.shouldBe(0)
+        calculatedValue shouldBe 0
     }
 
     "When speed is 0, returned value must be 0" {
         val calculatedValue = EVCalc.calculateMileage(75, 1800, 31, 100, 0, device)
-        calculatedValue.shouldBe(0)
+        calculatedValue shouldBe 0
     }
 
     "When battery capacity is greater than 100, value needs to be greater than 0" {
         val calculatedValue = EVCalc.calculateMileage(75, 101, 31, 100, 36, device)
-        calculatedValue.shouldNotBeLessThanOrEqual(0)
+        calculatedValue shouldNotBeLessThanOrEqual 0
     }
 
     "When start values are passed as the arguments, returned value needs to be 88"{
         val calculatedValue = EVCalc.calculateMileage(75, 1555, 25, 100, 35, device)
-        calculatedValue.shouldBe(88)
+        calculatedValue shouldBe 88
     }
 
     "If all values are totally real, returned value should be normal too" {
         val calculatedValue = EVCalc.calculateMileage(60, 1556, 31, 100, 36, device)
-        calculatedValue.shouldBeGreaterThan(80).shouldBeLessThan(90)
+        calculatedValue shouldBeGreaterThan 80
+        calculatedValue shouldBeLessThan 90
+    }
+
+    "Calculation speed needs to be lower than 0.1" {
+        val (_, duration) = executeAndMeasureTimeInMillis {
+            EVCalc.calculateMileage(60, 1556, 31, 100, 36, device)
+        }
+        duration shouldNotBeGreaterThan 100
     }
 })
