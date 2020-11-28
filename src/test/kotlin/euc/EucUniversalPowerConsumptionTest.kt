a847ae33a9a7456b019cbee018cd85ad6fa583f0
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-28 22:13:33 +0200

    make tests actually useful

--- src/test/kotlin/euc/EucUniversalPowerConsumptionTest.kt
+++ src/test/kotlin/euc/EucUniversalPowerConsumptionTest.kt
@@ -1,6 +1,8 @@
 package euc
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.ints.shouldBeLessThan
 import org.dynamium.evcalc.engine.api.DeviceModel
 import org.dynamium.evcalc.engine.api.EVCalc
 
@@ -8,6 +10,9 @@ class EucUniversalPowerConsumptionTest : StringSpec({
     val device = DeviceModel.EUC_UNIVERSAL
 
     "Basic calculation" {
-        println(EVCalc.calculatePowerConsumption(81, 1554, 31, 100, 44, 49, device))
+        val result = EVCalc.calculatePowerConsumption(81, 1554, 31, 100, 44, 49, device)
+
+        result shouldBeGreaterThan 60
+        result shouldBeLessThan 80
     }
 })
