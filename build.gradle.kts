50bfa9f0537d4563e5ebd8e16b841c8b2188ae22
Егор Яковенко <ggoraa1029@gmail.com>
2020-11-30 09:08:25 +0200

    update project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.jetbrains.dokka") version "1.4.10.2"
 }
 group = "org.dynamium.evcalc"
-version = "1.0-dev5"
+version = "1.0-dev6-SNAPSHOT"
 
 repositories {
     mavenCentral()
