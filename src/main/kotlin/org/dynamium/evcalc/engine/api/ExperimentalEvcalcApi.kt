cd70fac80a7b3f7e9f7870bfc328c1eadb96da45
Егор Яковенко <ggoraa1029@gmail.com>
2020-12-03 16:45:01 +0200

    renamed the experimental api annotation

--- src/main/kotlin/org/dynamium/evcalc/engine/api/ExperimentalEvcalcApi.kt
+++ src/main/kotlin/org/dynamium/evcalc/engine/api/ExperimentalEvcalcApi.kt
@@ -5,4 +5,4 @@ package org.dynamium.evcalc.engine.api
 @RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-annotation class ExperimentalApi
+annotation class ExperimentalEvcalcApi
