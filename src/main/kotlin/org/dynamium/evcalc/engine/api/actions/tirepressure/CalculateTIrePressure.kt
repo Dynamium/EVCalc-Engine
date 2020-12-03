08e0f5f28591b40b6ab88805c2df1f3127753eb2
Егор Яковенко <ggoraa1029@gmail.com>
2020-12-03 16:14:53 +0200

    add tire pressure calculation function

---
+++ src/main/kotlin/org/dynamium/evcalc/engine/api/actions/tirepressure/CalculateTIrePressure.kt
@@ -0,0 +1,13 @@
+@file:Suppress("unused")
+
+package org.dynamium.evcalc.engine.api.actions.tirepressure
+
+import org.dynamium.evcalc.engine.api.EVCalc
+
+fun EVCalc.calculateTirePressure(
+    wheelDiameter: Int,
+    riderWeight: Int,
+    rideSoftness: RideSoftness
+) {
+    
+}
