f1fc67c8ce9db763f5daab6ce7d13b78f537891e
Егор Яковенко <ggoraa1029@gmail.com>
2020-12-03 16:43:16 +0200

    add a tire calculation function in the core

---
+++ src/main/kotlin/org/dynamium/evcalc/engine/core/tirepressure/EucTirePressureCalculation.kt
@@ -0,0 +1,13 @@
+package org.dynamium.evcalc.engine.core.tirepressure
+
+import org.dynamium.evcalc.engine.api.calculation.tirepressure.RideSoftness
+import org.dynamium.evcalc.engine.api.calculation.tirepressure.TireType
+
+fun calculateEucTirePressure(
+    wheelDiameter: Int,
+    riderWeight: Int,
+    rideSoftness: RideSoftness,
+    tireType: TireType
+): Int {
+    return 0
+}
