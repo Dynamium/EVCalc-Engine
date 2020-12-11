27cd94a388910a351f62ed1fb73aa6487ff49ed8
Егор Яковенко <ggoraa1029@gmail.com>
2020-12-11 15:30:59 +0200

    refactor start values object

--- src/main/kotlin/org/dynamium/evcalc/engine/core/tirepressure/EucTirePressureCalculation.kt
+++ src/main/kotlin/org/dynamium/evcalc/engine/core/tirepressure/EucTirePressureCalculation.kt
@@ -7,14 +7,16 @@ import org.dynamium.evcalc.engine.api.calculation.tirepressure.TireType
 import org.dynamium.evcalc.engine.core.utils.math.round
 
 
-private object StartPressure {
-    const val tenInch = 2.5F
-    const val fourteenInch = 3.1F
-    const val sixteenInch = 2.8F
-    const val eighteenInch = 2.5F
-    const val twentyTwoInch = 1.5F
-}
+private object StartValues {
+    object Pressure {
+        const val tenInch = 2.5F
+        const val fourteenInch = 3.1F
+        const val sixteenInch = 2.8F
+        const val eighteenInch = 2.5F
+        const val twentyTwoInch = 1.5F
+    }
 
+}
 
 fun calculateEucTirePressure(
     wheelDiameter: Int,
@@ -28,13 +30,13 @@ fun calculateEucTirePressure(
     // ---------------- Step 1/?: Apply wheel diameter ---------------- //
 
     calculatedValue = when (wheelDiameter) {
-        10 -> StartPressure.tenInch
-        14 -> StartPressure.fourteenInch
-        15 -> ((StartPressure.fourteenInch + StartPressure.sixteenInch) / 2).round(2).toFloat()
-        16 -> StartPressure.sixteenInch
-        17 -> ((StartPressure.sixteenInch + StartPressure.eighteenInch) / 2).round(2).toFloat()
-        18 -> StartPressure.eighteenInch
-        22 -> StartPressure.twentyTwoInch
+        10 -> StartValues.Pressure.tenInch
+        14 -> StartValues.Pressure.fourteenInch
+        15 -> ((StartValues.Pressure.fourteenInch + StartValues.Pressure.sixteenInch) / 2).round(2).toFloat()
+        16 -> StartValues.Pressure.sixteenInch
+        17 -> ((StartValues.Pressure.sixteenInch + StartValues.Pressure.eighteenInch) / 2).round(2).toFloat()
+        18 -> StartValues.Pressure.eighteenInch
+        22 -> StartValues.Pressure.twentyTwoInch
         else -> {
             if (wheelDiameter < 14) {
 
